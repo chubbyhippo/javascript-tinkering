bc7c32f294250e3a124d25ed8a0295f882e24617
ch <mkmsacc@outlook.com>
2024-01-01 10:26:50 +0700

    Update ESLint configuration file
    The ESLint configuration file has been updated to import 'eslint-plugin-vitest' using require() instead of import. Moreover, 'vitest' has been added to the plugins array. This change ensures proper configuration and utilisation of 'vitest' during linting processes.

--- react-testing-library-vite-starter/.eslintrc.cjs
+++ react-testing-library-vite-starter/.eslintrc.cjs
@@ -1,5 +1,4 @@
-import vitest from "eslint-plugin-vitest";
-
+const vitest = require("eslint-plugin-vitest");
 module.exports = {
     root: true,
     env: {browser: true, es2020: true},
@@ -13,7 +12,7 @@ module.exports = {
     ignorePatterns: ['dist', '.eslintrc.cjs'],
     parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
     settings: {react: {version: '18.2'}},
-    plugins: ['react-refresh'],
+    plugins: ['react-refresh', 'vitest'],
     rules: {
         'react-refresh/only-export-components': [
             'warn',
