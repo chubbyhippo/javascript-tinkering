651cbfd4cc296e6035c59f3fceb369ed71ab97d8
chubbyhippo <mkmsacc@outlook.com>
2024-04-16 14:35:30 +0700

    Add calculateWinner function to App.js
    This commit adds a new function, calculateWinner, to the App component in the Tic Tac Toe tutorial project. This function checks all possible 3-in-a-row combinations and returns the winner if found. It's used to determine the game's outcome.

--- react-tutorial-tic-tac-toe/src/App.js
+++ react-tutorial-tic-tac-toe/src/App.js
@@ -48,3 +48,25 @@ export default function Board() {
     </>
   );
 }
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  lines.forEach(element => {
+    const [a, b, c] = element;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  });
+
+  return null;
+}
